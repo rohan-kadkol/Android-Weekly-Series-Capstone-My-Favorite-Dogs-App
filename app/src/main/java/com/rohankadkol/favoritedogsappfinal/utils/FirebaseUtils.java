6f21edc871f7ff4ebbb8d73080dee3138e09620d
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-08-28 17:23:46 -0500

    Added TODOs for step 2

--- app/src/main/java/com/rohankadkol/favoritedogsappfinal/utils/FirebaseUtils.java
+++ app/src/main/java/com/rohankadkol/favoritedogsappfinal/utils/FirebaseUtils.java
@@ -15,7 +15,19 @@ public final class FirebaseUtils {
     private FirebaseUtils() {}
 
     public static void addDog(String name, String breed, double age, String imageUrl, String notes, String likes, String dislikes) {
+        // TODO (1): Create a FirebaseDatabase instance and call it database
+        // FirebaseDatabase database = FirebaseDatabase.getInstance();
+        // TODO (2): Using this instance, create a DatabaseReference to the path "dogs". Name it "ref".
+        // DatabaseReference ref = database.getReference("dogs");
+        // TODO (3): Create a variable called key that stores the key of the new dog.
+        // String key = ref.push().getKey();
+        // TODO (4): Create a new Dog object using the above key the method parameters
+        // Dog dog = new Dog(key, name, breed, age, imageUrl, notes, likes, dislikes);
 
+        // TODO (5): If the key is not null, add the dog object to the "/dogs/<dogId>" path
+        // if (key != null) {
+        //     ref.child(key).setValue(dog);
+        // }
     }
 
     public static void editDog(Dog dog, String name, String breed, double age, String imageUrl, String notes, String likes, String dislikes) {
@@ -27,9 +39,21 @@ public final class FirebaseUtils {
         dog.setLikes(likes);
         dog.setDislikes(dislikes);
 
+        // TODO (6): Create a FirebaseDatabase instance and call it database
+        // FirebaseDatabase database = FirebaseDatabase.getInstance();
+        // TODO (7): Using this instance, create a DatabaseReference to the path "dogs". Name it "ref".
+        // DatabaseReference ref = database.getReference("dogs");
+
+        // TODO (8): Set the value of the "/dogs/<dogId>" path to the dog variable passed as a parameter
+        // ref.child(dog.getId()).setValue(dog);
     }
 
     public static void deleteDog(Dog dog) {
-
+        // TODO (9): Create a FirebaseDatabase instance and call it database
+        // FirebaseDatabase database = FirebaseDatabase.getInstance();
+        // TODO (10): Using this instance, create a DatabaseReference to the path "dogs/<dogId>". Name it "ref".
+        // DatabaseReference ref = database.getReference("dogs").child(dog.getId());
+        // TODO (11): Remove the value at the ref's path
+        // ref.removeValue();
     }
 }
